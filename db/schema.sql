bc62bd67386e376a329f7441381aef0e64fd2cb4
Richard Power <richardpwr271@gmail.com>
2025-11-09 16:49:26 -0500

    created user.js and changed needed code. fixed the schema and seeded the database.

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,13 @@
 DROP TABLE IF EXISTS playlists_tracks;
 DROP TABLE IF EXISTS playlists;
 DROP TABLE IF EXISTS tracks;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+  id serial PRIMARY KEY,
+  username text UNIQUE NOT NULL,
+  password text NOT NULL
+);
 
 CREATE TABLE tracks (
   id serial PRIMARY KEY,
@@ -11,7 +18,8 @@ CREATE TABLE tracks (
 CREATE TABLE playlists (
   id serial PRIMARY KEY,
   name text NOT NULL,
-  description text NOT NULL
+  description text NOT NULL,
+  user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE playlists_tracks (
